90a104d013a978512a016c64c96d3be8a6956e6e
Fábio Mendes <fabiobends@gmail.com>
2024-10-26 09:31:51 -0300

    chore: add style setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        primary: "#581C87",
+        secondary: "#F59E0B",
+        tertiary: "#FCD34D",
+        "light-purple": "#7E22CE",
+        "light-blue": "#0369A1",
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontSize: {
+        paragraph: "1.125rem",
+        heading: "3.5rem",
+      },
+      lineHeight: {
+        large: "1.8",
+        medium: "1.6",
+        normal: "1.4",
+        small: "1.1",
       },
     },
   },
