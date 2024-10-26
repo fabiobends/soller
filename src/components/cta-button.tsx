602a86ea11d2aa6679372eed4d8e57532f3157e2
Fábio Mendes <fabiobends@gmail.com>
2024-10-26 09:37:56 -0300

    feat: add components
    Add components that will be used in the app.

---
+++ src/components/cta-button.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Text } from "./text";
+
+const variants = {
+  purple: "border-primary text-primary",
+  yellow: "border-tertiary text-tertiary",
+};
+
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  title: string;
+  color?: keyof typeof variants;
+}
+
+const config = {
+  height: 24,
+  width: 24,
+};
+
+export function CtaButton({ title, color = "purple", ...props }: Props) {
+  const className = `${variants[color]} border-2 rounded-full flex justify-between h-16 w-[306px] items-center gap-2 px-6 ${props.className}`;
+
+  return (
+    <button {...props} className={className}>
+      <Text variant="highlight" className="text-center flex-1 leading-0">
+        {title}
+      </Text>
+      <Image
+        src={`/assets/icons/arrow-right-${color}.svg`}
+        alt="Arrow Right"
+        {...config}
+      />
+    </button>
+  );
+}
